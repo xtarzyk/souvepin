1c7c1f6fac3bd2e7ae092048d3dbb5262adbbb96
xtarzyk <xtarzyk@tutanota.com>
2024-07-02 20:45:40 +0200

    pages and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
-  const x = 1
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Product from "./pages/Product"
+import Homepage from "./pages/Homepage"
+import Pricing from "./pages/Pricing"
+import PageNotFound from "./pages/PageNotFound"
 
-  return (
-    <h1>
-      SouvePin
-    </h1>
-  )
+function App() {
+
+  return <BrowserRouter>
+    <Routes>
+      <Route path="product" element={<Product />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="pricing" element={<Pricing />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
