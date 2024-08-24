be80cd20b755ed5cce1e11af9bc900439b4d794f
xtarzyk <xtarzyk@tutanota.com>
2024-08-24 21:37:56 +0200

    react-leaflet map added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,13 +11,13 @@ module.exports = {
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
-  // rules: {
-  //   "no-unused-vars": ["warn"],
-  //   "react/jsx-no-target-blank": "off",
-  //   "react-refresh/only-export-components": [
-  //     "warn",
-  //     { allowConstantExport: true },
-  //   ],
-  //   "react/prop-types": 0,
-  // },
+  rules: {
+    "no-unused-vars": ["warn"],
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/prop-types": 0,
+  },
 };
